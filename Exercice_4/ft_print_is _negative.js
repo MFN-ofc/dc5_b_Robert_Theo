30b2fa361d40cec0fa5ec750322211043808d64e
Theo Robert <contact@theo-robert.fr>
2022-12-04 20:20:05 +0100

    last modifs

--- Exercice_4/ft_print_is _negative.js
+++ Exercice_4/ft_print_is _negative.js
@@ -2,7 +2,7 @@ let nombre = prompt("Entrer un nombre");
 nombre = parseInt(nombre);
 function afficherSigne(nombre) {
   if(Number.isInteger(nombre)){
-    if (nombre > 0) {
+    if (nombre >= 0) {
       document.write("P");
     } else if (nombre < 0) {
       document.write("N");
