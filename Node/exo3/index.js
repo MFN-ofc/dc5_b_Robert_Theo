d2bbaaf5610f7271ed18c92ef1ceae01b47664f4
Theo Robert <contact@theo-robert.fr>
2023-02-03 08:22:34 +0100

    big extraction 🏴‍☠️

--- Node/exo3/index.js
+++ Node/exo3/index.js
@@ -1,18 +1,27 @@
 const fs = require( 'fs');
 const createCsvParser = require ('csv-parser');
 const readStream = fs. createReadStream('/Users/theorobert/Desktop/dc5_b_Robert_Theo/Node/exo3/electronic-card-transactions-december-2022-csv-tables.csv');
-const writeStream = fs.createWriteStream('/Users/theorobert/Desktop/dc5_b_Robert_Theo/Node/exo3/data.csv');
+const writeStream = fs.createWriteStream('/Users/theorobert/Desktop/dc5_b_Robert_Theo/Node/exo3/result.csv');
 const parser = createCsvParser();
-parser.transform = function (record, callback) {
-const cleanedRecord = record
-.filter(field => field.trim() !== '') //Supprime les ligne vide
-.filter((_, index) => index !== 3) // supprime le 4e champs de l'enregistrement .map(field => field.trim()); // Supprime les espaces inutiles
-if (cleanedRecord. length === 4) {
-callback(null, cleanedRecord);
-} else {
-callback();
-}
-};
-readStream
-.pipe (parser)
-.pipe(writeStream);
+
+
+const columnsToKeep = ['Period', 'Data_value', 'Series_title_2'];
+writeStream.write('Id,Period,Data_value,Series_title_2\n');
+var id=0;
+fs.createReadStream('/Users/theorobert/Desktop/dc5_b_Robert_Theo/Node/exo3/electronic-card-transactions-december-2022-csv-tables.csv')
+  .pipe(createCsvParser())
+  .on('data', (data) => {
+    // data contient une ligne du fichier CSV parsé en objet JavaScript
+    // Extraire uniquement les colonnes qui vous intéressent
+    const filteredData = {};
+    columnsToKeep.forEach((colonne) => {
+      filteredData[colonne] = data[colonne];
+    });
+    // écrire les données filtrées dans un nouveau fichier
+    id++;
+    writeStream.write(`${id},${filteredData.Period},${filteredData.Data_value},${filteredData.Series_title_2}\n`);
+  })
+  .on('end', () => {
+    console.log('Extraction terminée');
+    writeStream.end();
+  });
